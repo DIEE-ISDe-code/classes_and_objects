e987410b7cd83ed9f2c90fd07bbb7901f568202d
Luca Didaci”git config --global user.email “didaci@gmail.com <didaci@gmail.com>
2019-11-12 10:24:10 +0100

    add Point_01 exercise

---
+++ points_01_p/main.py
@@ -0,0 +1,11 @@
+
+
+from point import Point
+
+
+p1=Point(0,2)
+p2=Point(3,4)
+print(p1.calculate_distance(p1))
+print(p1.calculate_distance(p2))
+print(p2.calculate_distance(p1))
+print(p2.calculate_distance(p2))
