6b12f93f84ce0491080c293b69f1e4bc4ca78f0b
Luca Didaci”git config --global user.email “didaci@gmail.com <didaci@gmail.com>
2019-11-21 10:23:23 +0100

    Add inheritance_01

---
+++ inheritance_01/mainfile.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import contact
+    
+    
+a=contact.Contact('pippo', 'pippo@gmail.com')
+b=contact.Supplier('pluto', 'pluto@gmail.com') 
+c=contact.Friend('paperino', 'pape@gmail.com', '131313') 
+d=contact.Friend('gastone', 'gasto.gmail.com', '777777') 
+
+
+print(a.who_I_am())
+print(b.who_I_am())
+print(c.who_I_am())
+print(d.who_I_am())
+print(d.phone)
