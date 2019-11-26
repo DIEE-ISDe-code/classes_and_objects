388b48c0caa9c2e3c936c8b005d12b29e3407f75
Luca Didaci”git config --global user.email “didaci@gmail.com <didaci@gmail.com>
2019-11-26 19:38:03 +0100

    Update car_composition.py

--- car_01/car_composition.py
+++ car_01/car_composition.py
@@ -5,6 +5,8 @@
 
 """
 
+import copy
+
 
 class Engine():
     def __init__(self,displacement=1000):
@@ -22,7 +24,7 @@ class Car():
     def __init__(self,engine,wheels, seats):
         self.engine=engine
         self.wheels= wheels  
-        self.seats=seats
+        self.seats=seats # copy, deepcopy
         
  
 engine=Engine()
@@ -37,7 +39,13 @@ print('\nSEATS')
 print(car.seats)
 print('\nWHEELS')
 print(car.wheels)
-print('\nPRESSURE')
+
+print('\nWHEEL - PRESSURE')
 print(car.wheels[0].pressure)
 wheels[0].pressure=10
 print(car.wheels[0].pressure)
+
+print('\nSEAT - COLOR')
+print(car.seats[0].color)
+seats[0].color=5
+print(car.seats[0].color)
