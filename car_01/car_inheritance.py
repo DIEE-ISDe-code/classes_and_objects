002416dcbae81d0c20eefef8b55e05fea1d10886
Luca Didaci”git config --global user.email “didaci@gmail.com <didaci@gmail.com>
2019-11-26 19:07:43 +0100

    add car_inheritance

---
+++ car_01/car_inheritance.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Abstract class forced all the subcasses to implement _post_init() method.
+
+
+"""
+
+
+class Engine():
+    def __init__(self,displacement=1000):
+        self.displacement=displacement
+
+class EngineWithWheels(Engine):
+    def __init__(self,displacement=1000, nWheels=4):
+        super().__init__(displacement)
+        self.nWheels=nWheels
+        
+class Car(EngineWithWheels):
+    def __init__(self,displacement=1000, nWheels=4, nSeats=5):
+        super().__init__(displacement, nWheels)
+        self.nSeats=nSeats
+        
+                
+a=Car(2000,5, 7)
+
+print(a.displacement)
+print(a.nWheels)
+print(a.nSeats)
+ 
